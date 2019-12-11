c38db34fc61dbaf7cde332bc0ea90c16a52fb11d
Luis Carrillo <juan_carrillog@my.unitec.edu.mx>
2019-12-11 11:01:33 -0600

    Add project files.

---
+++ Infrastructure/EmailSender.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class EmailSettings
+    {
+        public string Host { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+        public string Address { get; set; }
+        public string DisplayName { get; set; }
+        public string TargetName { get; set; }
+    }
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailSender(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            try
+            {
+                var smtp = new SmtpClient(_emailSettings.Host, _emailSettings.Port)
+                {
+                    EnableSsl = _emailSettings.EnableSsl,
+                    UseDefaultCredentials = false,
+                    Timeout = 10000,
+                    Credentials = new NetworkCredential(_emailSettings.UserName, _emailSettings.Password),
+                    TargetName = _emailSettings.TargetName,
+                    DeliveryMethod = SmtpDeliveryMethod.Network
+                };
+
+                smtp.ServicePoint.MaxIdleTime = 1;
+
+                string address = _emailSettings.Address;
+                string displayName = _emailSettings.DisplayName;
+
+                var from = new MailAddress(address, displayName, Encoding.UTF8);
+                var to = new MailAddress(email);
+
+                var message = new MailMessage(from, to)
+                {
+                    Subject = subject,
+                    SubjectEncoding = Encoding.UTF8,
+                    IsBodyHtml = true,
+                    BodyEncoding = Encoding.UTF8,
+                    Body = htmlMessage
+                };
+
+                message.Bcc.Add("info@d3v.mx");
+
+                //message.To.Add(to);
+
+                await smtp.SendMailAsync(message);
+                message.Dispose();
+            }
+            catch (Exception ex)
+            {
+                // TODO: handle exception
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public HomeController(ILogger<HomeController> logger, IEmailSender emailSender)
+        {
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Mail(string id)
+        {
+            var callbackUrl = Url.Link("Default", new { Controller = "Home", Action = "Index"});
+            var htmlMessage = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"//www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" +
+                "<html lang=\"es\" xmlns=\"//www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\"><head>" +
+                "    <title></title>" +
+                "    <meta charset=\"utf-8\" /> <!-- utf-8 works for most cases -->" +
+                "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />" +
+                "    <meta name=\"viewport\" content=\"width=device-width\" /> <!-- Forcing initial-scale shouldn't be necessary -->" +
+                "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" /> <!-- Use the latest (edge) version of IE rendering engine -->" +
+                "    <meta name=\"x-apple-disable-message-reformatting\" />  <!-- Disable auto-scale in iOS 10 Mail entirely -->" +
+                $"</head><body><h1>Titulo</h1><a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>Ver los detalles</a></body></html>";
+
+            await _emailSender.SendEmailAsync(id, "Subjet", htmlMessage);
+
+            return Content("Enviado");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
